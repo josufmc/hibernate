7af515209332d1fc3618009bea8a941bf5ffddd4
Josu <josufmc@hotmail.com>
2016-10-06 08:58:45 +0200

    Tests in the Country and their languages relationships

--- src/main/java/com/gurenet/HibernateTest/model/CountryLanguagePK.java
+++ src/main/java/com/gurenet/HibernateTest/model/CountryLanguagePK.java
@@ -3,16 +3,19 @@ package com.gurenet.HibernateTest.model;
 import java.io.Serializable;
 
 import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Embeddable
 public class CountryLanguagePK implements Serializable{
-	/**
-	 * 
-	 */
+
+	
 	private static final long serialVersionUID = 6764088904472411903L;
 	
 	private String language;
-	private String countryCode;
+	@ManyToOne
+	@JoinColumn(name="CountryCode")
+	private Country country;
 	
 	public String getLanguage() {
 		return language;
@@ -20,11 +23,11 @@ public class CountryLanguagePK implements Serializable{
 	public void setLanguage(String language) {
 		this.language = language;
 	}
-	public String getCountryCode() {
-		return countryCode;
+	public Country getCountry() {
+		return country;
 	}
-	public void setCountryCode(String countryCode) {
-		this.countryCode = countryCode;
+	public void setCountry(Country country) {
+		this.country = country;
 	}
 		
 }
