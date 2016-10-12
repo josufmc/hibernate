ffb2dcd924a97f48a15f6fe32a2bd79ee9406a4e
josufmc <josufmc@hotmail.com>
2016-10-12 14:57:24 +0200

    Fixed problem with lv2 cache provider

--- src/main/java/com/gurenet/HibernateTest/App.java
+++ src/main/java/com/gurenet/HibernateTest/App.java
@@ -14,22 +14,33 @@ import com.gurenet.HibernateTest.model.Country;
 import com.gurenet.HibernateTest.model.CountryLanguage;
 
 /**
- * Hello world!
- *
+ * Aplicación de prueba para las cachés L1 y L2 de Hibernate
  */
 public class App {
+	
+	/**
+	 * Nota: Para probar la aplicación, debe estar cargada la base 
+	 * de datos world en localhost de MySQL que se ha utilizado en clase
+	 */
 	public static void main(String[] args) {
-		//showAllCountries();
-		//testCacheL1();
-		testCacheL2();
+		
+		/* Descomentar un método u otro para las pruebas. 
+		 * Cada método tiene internamente comentarios sobre 
+		 * las pruebas y qué ocurre en cada test.
+		 * */
+		testCacheL1();
+		//testCacheL2();
 	}
 	
-	
+	/**
+	 * Prueba de caché L1
+	 */
 	public static void testCacheL1(){
 		Configuration cfg = new Configuration()
 				.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver")
 				.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/world")
 				.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+				.setProperty("hibernate.cache.use_second_level_cache", "false")
 				.setProperty("hibernate.connection.username", "root").setProperty("hibernate.connection.password", "")
 				.setProperty("hibernate.show_sql", "true")
 				.setProperty("hibernate.current_session_context_class", "thread")
@@ -69,15 +80,21 @@ public class App {
 		System.out.println("OK!");
 	}
 	
+	/**
+	 * Prueba de caché L2
+	 */
 	public static void testCacheL2(){
+		
 		Configuration cfg = new Configuration()
 				.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver")
 				.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/world")
 				.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
-				// Metemos la caché más simple para el ejemplo (ConcurrentHashMap)
+				// Metemos la EHCache para caché de nivel 2
 				.setProperty("hibernate.cache.use_second_level_cache", "true")
 				.setProperty("hibernate.cache.provider_class", "org.hibernate.cache.EhCacheProvider")
-				.setProperty("hibernate.cache.region.factory_class", "org.hibernate.cache.ehcache.EhCacheRegionFactory")
+				.setProperty("hibernate.cache.region.factory_class", "org.hibernate.cache.EhCacheRegionFactory")
+				//.setProperty("hibernate.cache.provider_class", "net.sf.ehcache.hibernate.EhCacheProvider")
+				//.setProperty("hibernate.cache.region.factory_class", "net.sf.ehcache.hibernate.EhCacheRegionFactory")
 				.setProperty("hibernate.connection.username", "root").setProperty("hibernate.connection.password", "")
 				.setProperty("hibernate.show_sql", "true")
 				.setProperty("hibernate.current_session_context_class", "thread")
@@ -97,11 +114,14 @@ public class App {
 		s2.beginTransaction();
 		
 		// Obtenemos el país 4 veces, podemos ver que hibernate 
-		// sólo lanza la consulta una vez en el grupo de las sessión s. 
+		// sólo lanza la consulta la primera vez. 
 		Country country1 = (Country)s.load(Country.class, "NLD");
 		Country country2 = (Country)s.load(Country.class, "NLD");
 		Country country3 = (Country)s.load(Country.class, "NLD");
-		// Si cambiamos de sesión, vuelve a realizar la consulta. 
+		
+		// Si cambiamos de sesión, tampoco hace la consulta 
+		// ya que hemos marcado la entidad como cacheable 
+		// (Está en la caché de L2). 
 		Country country4 = (Country)s2.load(Country.class, "NLD");
 		
 		// Terminamos las transacciones
@@ -117,11 +137,15 @@ public class App {
 		System.out.println("OK!");
 	}
 	
+	/**
+	 * Mostramos los paises normalmente
+	 */
 	public static void showAllCountries(){
 		Configuration cfg = new Configuration()
 				.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver")
 				.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/world")
 				.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+				.setProperty("hibernate.cache.use_second_level_cache", "false")
 				.setProperty("hibernate.connection.username", "root").setProperty("hibernate.connection.password", "")
 				//.setProperty("hibernate.show_sql", "true")
 				.setProperty("hibernate.current_session_context_class", "thread")
@@ -139,6 +163,7 @@ public class App {
 		//c.setFetchMode("cities", FetchMode.SELECT);
 		c.setFetchMode("cities", FetchMode.JOIN);	// Lo ponemos así porque mejor que saque más datos que varias consultas
 		c.setFetchMode("capital", FetchMode.JOIN);
+		@SuppressWarnings("unchecked")
 		List<Country> countries = c.list();
 		for(Country country: countries){
 			City capital = country.getCapital();
